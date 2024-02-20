892f4119eb0538f93dcbafce396952b9a7496073
mariarojas89 <88989805+mariarojas89@users.noreply.github.com>
2024-02-20 20:47:32 -0300

    Add files via upload

---
+++ PreEntrega1+Rojas-mariaBeatriz/javascript/valorar-clase.js
@@ -0,0 +1,23 @@
+alert ('Valoremos el performance de nuestro amigo pepe, estas listo?')
+let valorar;
+for (let i = 1; i<= 3; i++){
+    valorar = (prompt('Del 1 al 10, valoremos a pepe en las clases de javascript'))
+
+    if(valorar == 10){
+        alert('VAMO CAMPEON PEPE PRESIDENTE!!!')
+        break
+    }
+
+    else 
+        alert('Dale... Ponele voluntad')
+
+}
+
+if(valorar !=10){
+    alert('Como le fallamos al pobre pepe')
+}
+
+
+
+
+
